a1a1760e981bab263bb01c96666f4522c8c3dac1
None <hata.sdj.kisarazu@gmail.com>
2017-08-02 10:19:01 +0000

    リザルトページの作成

--- app/controllers/jankens_controller.rb
+++ app/controllers/jankens_controller.rb
@@ -65,6 +65,7 @@ class JankensController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_janken
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :jankens
-
+  get '/janken/result'
   resources :games
   resources :uranais
   resources :users
