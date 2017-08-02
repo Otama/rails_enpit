930df4b59c396c9cf7c17f48e88dd24911358cd1
None <hata.sdj.kisarazu@gmail.com>
2017-08-02 10:26:24 +0000

    /jankens/resultにアクセスできる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get '/jankens/result' => 'jankens#result'
   resources :jankens
-  get '/janken/result'
   resources :games
   resources :uranais
   resources :users
