7db66e52d5c79ca51c9d1aed53e39b4fa55a4b6d
Kozo Komiya <kozo.komiya@gmail.com>
2017-08-09 10:13:50 +0000

    update top page button

--- db/schema.rb
+++ db/schema.rb
@@ -48,12 +48,6 @@ ActiveRecord::Schema.define(version: 20170805114924) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "title"
